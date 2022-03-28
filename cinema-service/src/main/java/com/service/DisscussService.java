2f495cb55eee821bb4276755b1983f0fbfdfe00d
MIEaster <3109779869@qq.com>
2022-03-28 11:20:28 +0800

    修改CustomerDao 添加部分service

---
+++ cinema-service/src/main/java/com/service/DisscussService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import com.entity.Discuss;
+
+import java.util.List;
+
+public interface DisscussService {
+    List<Discuss> all();
+    boolean delete(int id);
+    int add(Discuss discuss);
+}
---
+++ cinema-service/src/main/java/com/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,41 @@
+package com.service.impl;
+
+import com.dao.CustomerDao;
+import com.entity.Customer;
+import com.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerDao dao;
+
+    @Override
+    public int addUser(Customer customer) {
+        return dao.addUser(customer);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return dao.delete(id);
+    }
+
+    @Override
+    public List<Customer> all() {
+        return dao.all();
+    }
+
+    @Override
+    public Customer getCustomerById(int id) {
+        return dao.getCustomerById(id);
+    }
+
+    @Override
+    public int updateCustomer(Customer customer) {
+        return dao.updateCustomer(customer);
+    }
+}
